eba5e3849b721975cb209fe4aa847e8817faa90a
Kush Saini <kushsaini.700@gmail.com>
2017-07-14 15:58:20 +0530

    service commit

--- Services/app/src/main/java/com/ks/services/MainActivity.java
+++ Services/app/src/main/java/com/ks/services/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ks.services;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Intent intent = new Intent();
+        intent.setClass(this,MyService.class);
+        Button startBtn = (Button) findViewById(R.id.startBtn);
+        Button stopBtn = (Button) findViewById(R.id.stopBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(intent);
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(intent);
+            }
+        });
     }
 }
---
+++ Services/app/src/main/java/com/ks/services/MyService.java
@@ -0,0 +1,32 @@
+package com.ks.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by ks on 14/7/17.
+ */
+
+public class MyService extends Service{
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "service Stopping", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+}
